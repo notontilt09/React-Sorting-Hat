64dd62cbee8abb6adda9e16c6f5bd2f29c274985
Daniel Weinman <namniewd@gmail.com>
2019-01-10 20:11:08 -0500

    question inputs all work and lead to next question

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -17,10 +17,13 @@ class App extends Component {
 
   sortMe = () => {
     const intro = document.querySelector('.intro');
-    const quiz= document.querySelector('.quiz');
-    const question2 = document.querySelector('.question2');
-    question2.classList.add('hide');
+    const question1= document.querySelector('.question1');
+    const quiz = document.querySelector('.quiz');
+    const questions = document.querySelectorAll('section');
+    console.log(questions);
+    questions.forEach(question => question.classList.add('hide'));
     quiz.classList.add('show');
+    question1.classList.remove('hide');
     intro.classList.add('hide');
   }
 
@@ -54,8 +57,152 @@ class App extends Component {
     }
     question1.classList.add('hide');
     question2.classList.remove('hide');
-
   } 
+
+  answerQuestion2 = event => {
+    event.preventDefault();
+    const form2 = document.querySelector('.form1');
+    const question2 = document.querySelector('.question2');
+    const question3 = document.querySelector('.question3');
+    for (let i = 0; i < form2.length; i++) {
+      if (form2[i].checked) {
+        switch (form2[i].value) {
+          case 'gryffindor':
+            this.setState({...this.state, gryffindor: this.state.gryffindor + 10})
+            break;
+          case 'hufflepuff':
+          this.setState({...this.state, hufflepuff: this.state.hufflepuff + 10})
+          break;
+          case 'ravenclaw':
+            this.setState({...this.state, ravenclaw: this.state.ravenclaw + 10})
+            break;
+          case 'slytherin':
+            this.setState({...this.state, slytherin: this.state.slytherin + 10})
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    question2.classList.add('hide');
+    question3.classList.remove('hide');
+  } 
+
+  answerQuestion3 = event => {
+    event.preventDefault();
+    const form3 = document.querySelector('.form3');
+    const question3 = document.querySelector('.question3');
+    const question4 = document.querySelector('.question4');
+    for (let i = 0; i < form3.length; i++) {
+      if (form3[i].checked) {
+        switch (form3[i].value) {
+          case 'gryffindor':
+            this.setState({...this.state, gryffindor: this.state.gryffindor + 10})
+            break;
+          case 'hufflepuff':
+          this.setState({...this.state, hufflepuff: this.state.hufflepuff + 10})
+          break;
+          case 'ravenclaw':
+            this.setState({...this.state, ravenclaw: this.state.ravenclaw + 10})
+            break;
+          case 'slytherin':
+            this.setState({...this.state, slytherin: this.state.slytherin + 10})
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    question3.classList.add('hide');
+    question4.classList.remove('hide');
+  }
+
+  answerQuestion4 = event => {
+    event.preventDefault();
+    const form4 = document.querySelector('.form4');
+    const question4 = document.querySelector('.question4');
+    const question5 = document.querySelector('.question5');
+    for (let i = 0; i < form4.length; i++) {
+      if (form4[i].checked) {
+        switch (form4[i].value) {
+          case 'gryffindor':
+            this.setState({...this.state, gryffindor: this.state.gryffindor + 10})
+            break;
+          case 'hufflepuff':
+          this.setState({...this.state, hufflepuff: this.state.hufflepuff + 10})
+          break;
+          case 'ravenclaw':
+            this.setState({...this.state, ravenclaw: this.state.ravenclaw + 10})
+            break;
+          case 'slytherin':
+            this.setState({...this.state, slytherin: this.state.slytherin + 10})
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    question4.classList.add('hide');
+    question5.classList.remove('hide');
+  }
+
+  answerQuestion5 = event => {
+    event.preventDefault();
+    const form5 = document.querySelector('.form5');
+    const question5 = document.querySelector('.question5');
+    const question6 = document.querySelector('.question6');
+    for (let i = 0; i < form5.length; i++) {
+      if (form5[i].checked) {
+        switch (form5[i].value) {
+          case 'gryffindor':
+            this.setState({...this.state, gryffindor: this.state.gryffindor + 10})
+            break;
+          case 'hufflepuff':
+          this.setState({...this.state, hufflepuff: this.state.hufflepuff + 10})
+          break;
+          case 'ravenclaw':
+            this.setState({...this.state, ravenclaw: this.state.ravenclaw + 10})
+            break;
+          case 'slytherin':
+            this.setState({...this.state, slytherin: this.state.slytherin + 10})
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    question5.classList.add('hide');
+    question6.classList.remove('hide');
+  }
+
+  answerQuestion6 = event => {
+    event.preventDefault();
+    const form6 = document.querySelector('.form6');
+    const question6 = document.querySelector('.question6');
+    const results = document.querySelector('.results');
+    for (let i = 0; i < form6.length; i++) {
+      if (form6[i].checked) {
+        switch (form6[i].value) {
+          case 'gryffindor':
+            this.setState({...this.state, gryffindor: this.state.gryffindor + 10})
+            break;
+          case 'hufflepuff':
+          this.setState({...this.state, hufflepuff: this.state.hufflepuff + 10})
+          break;
+          case 'ravenclaw':
+            this.setState({...this.state, ravenclaw: this.state.ravenclaw + 10})
+            break;
+          case 'slytherin':
+            this.setState({...this.state, slytherin: this.state.slytherin + 10})
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    question6.classList.add('hide');
+    results.classList.remove('hide');
+  }
 
   render() {
     return (
--- react-sorting-hat/src/Quiz.js
+++ react-sorting-hat/src/Quiz.js
@@ -29,6 +29,57 @@ const Quiz = props => {
                     </form>
                 </div>
             </section>
+            <section className="question3">
+                <h1>Question 3: Which of the follow describes a perfect afternoon for you?</h1>
+                <div className="answers">
+                    <form className="form3" onSubmit={props.answerQuestion3}>
+                        <input type="radio" name="quiz1" id="ravenclaw" value="ravenclaw" /><label htmlFor="ravenclaw">Studying of course!</label><br />
+                        <input type="radio" name="quiz1" id="slytherin" value="slytherin" /><label htmlFor="slytherin">Setting up an elaborate prank on my enemy.</label><br /> 
+                        <input type="radio" name="quiz1" id="gryffindor" value="gryffindor" /><label htmlFor="gryffindor">Fighting dragons or three-headed dogs.</label><br />
+                        <input type="radio" name="quiz1" id="hufflepuff" value="hufflepuff" /><label htmlFor="hufflepuff">I just hope I make it to the afternoon.</label><br /> 
+                        <button type="Submit" className="next-question">Next Question!</button>
+                    </form>
+                </div>
+            </section>
+            <section className="question4">
+                <h1>Question 4: Which of the follow describes a perfect afternoon for you?</h1>
+                <div className="answers">
+                    <form className="form4" onSubmit={props.answerQuestion4}>
+                        <input type="radio" name="quiz1" id="ravenclaw" value="ravenclaw" /><label htmlFor="ravenclaw">Studying of course!</label><br />
+                        <input type="radio" name="quiz1" id="slytherin" value="slytherin" /><label htmlFor="slytherin">Setting up an elaborate prank on my enemy.</label><br /> 
+                        <input type="radio" name="quiz1" id="gryffindor" value="gryffindor" /><label htmlFor="gryffindor">Fighting dragons or three-headed dogs.</label><br />
+                        <input type="radio" name="quiz1" id="hufflepuff" value="hufflepuff" /><label htmlFor="hufflepuff">I just hope I make it to the afternoon.</label><br /> 
+                        <button type="Submit" className="next-question">Next Question!</button>
+                    </form>
+                </div>
+            </section>
+            <section className="question5">
+                <h1>Question 5: Which of the follow describes a perfect afternoon for you?</h1>
+                <div className="answers">
+                    <form className="form5" onSubmit={props.answerQuestion5}>
+                        <input type="radio" name="quiz1" id="ravenclaw" value="ravenclaw" /><label htmlFor="ravenclaw">Studying of course!</label><br />
+                        <input type="radio" name="quiz1" id="slytherin" value="slytherin" /><label htmlFor="slytherin">Setting up an elaborate prank on my enemy.</label><br /> 
+                        <input type="radio" name="quiz1" id="gryffindor" value="gryffindor" /><label htmlFor="gryffindor">Fighting dragons or three-headed dogs.</label><br />
+                        <input type="radio" name="quiz1" id="hufflepuff" value="hufflepuff" /><label htmlFor="hufflepuff">I just hope I make it to the afternoon.</label><br /> 
+                        <button type="Submit" className="next-question">Next Question!</button>
+                    </form>
+                </div>
+            </section>
+            <section className="question6">
+                <h1>Question 6: Which of the follow describes a perfect afternoon for you?</h1>
+                <div className="answers">
+                    <form className="form6" onSubmit={props.answerQuestion6}>
+                        <input type="radio" name="quiz1" id="ravenclaw" value="ravenclaw" /><label htmlFor="ravenclaw">Studying of course!</label><br />
+                        <input type="radio" name="quiz1" id="slytherin" value="slytherin" /><label htmlFor="slytherin">Setting up an elaborate prank on my enemy.</label><br /> 
+                        <input type="radio" name="quiz1" id="gryffindor" value="gryffindor" /><label htmlFor="gryffindor">Fighting dragons or three-headed dogs.</label><br />
+                        <input type="radio" name="quiz1" id="hufflepuff" value="hufflepuff" /><label htmlFor="hufflepuff">I just hope I make it to the afternoon.</label><br /> 
+                        <button type="Submit" className="next-question">Next Question!</button>
+                    </form>
+                </div>
+            </section>
+            <section className="results">
+                <h1>Results</h1>
+            </section>
         </div>
     );
 }
